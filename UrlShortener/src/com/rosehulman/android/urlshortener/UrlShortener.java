f502e50a64711ecdd5a79e48b5babb6c4bf80982
Jimmy Theis <jimmy@jetheis.com>
2011-05-11 01:39:32 -0400

    Finished
    Signed-off-by: Jimmy Theis <jimmy@jetheis.com>

--- UrlShortener/src/com/rosehulman/android/urlshortener/UrlShortener.java
+++ UrlShortener/src/com/rosehulman/android/urlshortener/UrlShortener.java
@@ -2,17 +2,38 @@ package com.rosehulman.android.urlshortener;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class UrlShortener extends Activity {
-    
-    UrlService mUrls;
     
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        mUrls = new GooGlService();
+        final UrlService urlService = new GooGlService();
+        
+        final Button shortenButton = (Button) findViewById(R.id.shorten_button);
+        final Button viewsButton = (Button) findViewById(R.id.views_button);
+        final EditText url = (EditText) findViewById(R.id.input);
+        final TextView views = (TextView) findViewById(R.id.views);
+        
+        shortenButton.setOnClickListener(new OnClickListener() {
+            
+            public void onClick(View v) {
+                url.setText(urlService.getShortenedUrl(url.getText().toString()));
+            }
+        });
+        
+        viewsButton.setOnClickListener(new OnClickListener() {
+            
+            public void onClick(View v) {
+                views.setText(urlService.getNumberOfViews(url.getText().toString()) + " views");
+            }
+        });
     }
 }
